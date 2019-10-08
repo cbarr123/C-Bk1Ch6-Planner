d7d4e52f93ea7e26bc727f1db2455453d1e322ba
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-08 11:36:45 -0500

    initial commit

---
+++ Building.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Building
+{
+    class Building
+    {
+        private string _designer = "Carl";
+        private DateTime _dateConstructed = 2019-09-08;
+        private string _address = "building address";
+        private string _owner = "owner name";
+        public int Stories = 5;
+        public double Width = 30;
+        public double Depth = 50;
+        
+    }
+}
