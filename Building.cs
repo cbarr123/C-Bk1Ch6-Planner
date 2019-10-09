e9acc5304589fab67e7d3d6aeb7be753c6410e99
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-09 08:41:24 -0500

    Adding Building

--- Building.cs
+++ Building.cs
@@ -1,16 +1,52 @@
 using System;
 
-namespace Building
+namespace Planner
 {
-    class Building
+    public class Building
     {
-        private string _designer = "Carl";
-        private DateTime _dateConstructed = 2019-09-08;
-        private string _address = "building address";
-        private string _owner = "owner name";
+        //Private Fields
+        private string _designer = "Carl Barringer";
+        private DateTime _dateConstructed = new DateTime();
+        private string _address;
+        private string _owner;
+        //Public Properties
         public int Stories = 5;
         public double Width = 30;
         public double Depth = 50;
+        public double Volume 
+            {
+                get 
+                {
+                    return Width * Depth * (3 * Stories);
+                }
+            }
         
+        //Constructor for Building
+        public Building (string address) 
+        {
+            _address = address;
+        }
+
+
+        //Public Methods
+        public DateTime Construct() 
+        {
+            return _dateConstructed = DateTime.Now;
+        }
+        public void Purchase (string nameOfPurchaser)
+        {
+            _owner = nameOfPurchaser;
+        }
+
+        public void Print() 
+        {
+            Console.WriteLine(this._address);
+            Console.WriteLine("------------------------");
+            Console.WriteLine($"Constructed on ");
+            Console.WriteLine($"Owned by {this._owner}");
+
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace Planner
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Building 1
+            Building FiveOneTwoEight = new Building("512 8th Avenue");
+            //Give each building a width, height, and number of stories.
+            Console.WriteLine(FiveOneTwoEight);
+            FiveOneTwoEight.Stories = 6;
+            FiveOneTwoEight.Width = 20;
+            FiveOneTwoEight.Depth = 30;
+            Double FiveOneTwoEightVolume = FiveOneTwoEight.Volume;
+            //Construct each building
+            FiveOneTwoEight.Construct();
+            //Have business people in your city purchase each of your buildings
+            FiveOneTwoEight.Purchase("John Vanderbilt");
         }
     }
 }
